031f5ce1f2832a37b4040c74941a0f5c0c4e87d7
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-05-06 09:54:18 +0300

    edit: project list

--- client/src/components/ProjectList.tsx
+++ client/src/components/ProjectList.tsx
@@ -7,23 +7,15 @@ interface Props {
 }
 
 const ProjectList = ({ items }: Props) => {
-  return items.map((item) => <ProjectListItem key={item._id} item={item} />);
-};
-
-interface ProjectListItemProps {
-  item: Project;
-}
-
-const ProjectListItem = ({ item }: ProjectListItemProps) => {
-  return (
-    <Link href={`/projects/${item.slug}`}>
+  return items.map((item) => (
+    <Link key={item._id} href={`/projects/${item.slug}`}>
       <div className="mt-2 max-w-2xl p-4 border-2 border-solid border-black rounded hover:bg-slate-200 transition ease-in-out duration-500 cursor-pointer">
         <span className="font-semibold">{item.name}</span>
 
         <p className="mt-2 line-clamp-3">{item.description}</p>
       </div>
     </Link>
-  );
+  ));
 };
 
 export default ProjectList;
