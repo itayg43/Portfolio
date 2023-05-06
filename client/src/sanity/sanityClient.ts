627f2842501d6a054e116db7ffbf12b8b8325985
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-05-06 03:45:51 +0300

    edit: sanity client

--- client/src/sanity/sanityClient.ts
+++ client/src/sanity/sanityClient.ts
@@ -4,7 +4,7 @@ const sanityClient = createClient({
   projectId: "zpacskvu",
   dataset: "production",
   apiVersion: "2023-05-04",
-  useCdn: true,
+  useCdn: process.env.NODE_ENV === "production",
 });
 
 export default sanityClient;
