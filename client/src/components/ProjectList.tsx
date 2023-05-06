ef4bacd28f4c54d0b446b61fe28851363d2d0e64
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-05-06 10:04:17 +0300

    edit: project list

--- client/src/components/ProjectList.tsx
+++ client/src/components/ProjectList.tsx
@@ -7,15 +7,19 @@ interface Props {
 }
 
 const ProjectList = ({ items }: Props) => {
-  return items.map((item) => (
-    <Link key={item._id} href={`/projects/${item.slug}`}>
-      <div className="mt-2 max-w-2xl p-4 border-2 border-solid border-black rounded hover:bg-slate-200 transition ease-in-out duration-500 cursor-pointer">
-        <span className="font-semibold">{item.name}</span>
+  return (
+    <>
+      {items.map((item) => (
+        <Link key={item._id} href={`/projects/${item.slug}`}>
+          <div className="mt-2 max-w-2xl p-4 border-2 border-solid border-black rounded hover:bg-slate-200 transition ease-in-out duration-500 cursor-pointer">
+            <span className="font-semibold">{item.name}</span>
 
-        <p className="mt-2 line-clamp-3">{item.description}</p>
-      </div>
-    </Link>
-  ));
+            <p className="mt-2 line-clamp-3">{item.description}</p>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default ProjectList;
